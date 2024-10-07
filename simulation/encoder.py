e44331f0a8813d38e58818f7a68269b5e9e93d43
Eoin Murray <me@eoinmurray.info>
2024-10-07 14:23:20 +0200

    stash

--- simulation/encoder.py
+++ simulation/encoder.py
@@ -9,27 +9,19 @@ from vary_parameters import generate_parameter_variators
 from generate import generate_data, generate_qd_data
 import matplotlib.pyplot as plt
 
-# Set the latent space size variable
-latent_space_size = 32  # You can change this value to any desired latent space size
-
-# Generate data
-num_samples = 200
+latent_space_size = 128
+num_epochs = 200
+num_samples = 100
 seq_length = 1000
+batch_size = 32
+
 data = generate_data(num_samples, seq_length)
 qd_data, parameters = generate_qd_data(num_samples, seq_length)
-print(data.shape, qd_data.shape)
 data = qd_data
-
-# Data standardization (mean-zero normalization)
 data_mean = data.mean()
 data_std = data.std()
 data = (data - data_mean) / data_std
-
-# Convert data to PyTorch tensors
 data = torch.tensor(data, dtype=torch.float32).unsqueeze(1)  # Shape: (batch_size, 1, seq_length)
-
-# Create DataLoader
-batch_size = 32
 dataset = TensorDataset(data, data)  # Target is same as input for autoencoder
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
@@ -133,9 +125,6 @@ model = Conv1dAutoEncoder(seq_length=seq_length, latent_space_size=latent_space_
 # Define loss function and optimizer
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-# Training loop with dual loss
-num_epochs = 2000
 for epoch in range(num_epochs):
     running_loss_with_skip = 0.0
     running_loss_no_skip = 0.0
@@ -183,6 +172,7 @@ test_outputs_no_skip_np = test_outputs_no_skip.squeeze().detach().numpy()
 n = 5  # Number of samples to plot
 for i in range(n):
     plt.figure(figsize=(12, 4))
+    plt.xlim([1583.5, 1587.5])
     plt.plot(parameters['energies'], test_inputs_np[i], label='Original', linestyle='--')
     plt.plot(parameters['energies'], test_outputs_with_skip_np[i], label='Reconstructed (with skip connections)', alpha=0.7)
     plt.plot(parameters['energies'], test_outputs_no_skip_np[i], label='Reconstructed (from latent only)', alpha=0.7)
